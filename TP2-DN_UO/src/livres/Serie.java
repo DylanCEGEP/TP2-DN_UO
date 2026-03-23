518dfcd5e01f6ce1e48495eaa82a1ee47d615603
Dylan Noël <1865375@cegeplimoilou.ca>
2026-03-23 12:31:48 -0400

    ajout de la classe Serie (attribut + constructeur )

---
+++ TP2-DN_UO/src/livres/Serie.java
@@ -0,0 +1,18 @@
+package livres;
+
+import java.util.ArrayList;
+
+public class Serie {
+
+
+    private String nom;
+    private ArrayList<Ouvrage> listeOuvrage;
+
+    public Serie(String nom)
+    {
+        this.nom = nom;
+    }
+
+    
+
+}
